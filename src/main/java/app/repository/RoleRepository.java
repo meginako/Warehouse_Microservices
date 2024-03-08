7500a9106d217e7a67349b15f301b88d81598484
Megi Nako <mnako19@epoka.edu.al>
2024-03-08 23:13:34 +0100

    add spring security

---
+++ src/main/java/app/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package app.repository;
+
+import app.model.ERole;
+import app.model.Role;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+  Optional<Role> findByName(ERole name);
+}
